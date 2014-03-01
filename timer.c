f6e8a3479a57fa7378193dc6d8b9ac7417952e91
Reuben Hawkins <reubenhwk@gmail.com>
2014-02-28 21:32:53 -0600

    timer.c: prune expired
    coverage testing exposed the function, expired, as never called.

--- timer.c
+++ timer.c
@@ -43,11 +43,3 @@ int next_time_msec(struct Interface const *iface)
 	return retval >= 1 ? retval : 1;
 }
 
-int expired(struct Interface const *iface)
-{
-	struct timeval tv;
-	gettimeofday(&tv, NULL);
-	if (timevaldiff(&iface->next_multicast, &tv) > 0)
-		return 0;
-	return 1;
-}
